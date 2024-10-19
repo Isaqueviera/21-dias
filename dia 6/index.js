ffe839fb2d1926994239c6b03932609358515fac
isaque vieira <isaque>
2024-10-19 20:40:25 -0300

    dia 6

---
+++ dia 6/index.js
@@ -0,0 +1,23 @@
+let numero = Number(prompt('Escolha um numero:'))
+numero2 = numero + 1
+numero3 = numero + 2
+
+for(let contador = 0; contador <= 5 ; contador++
+){
+    console.log(
+        numero + "x" + contador + '=' + numero*contador)}
+
+        console.log('==================')
+
+for(let contador = 0; contador <= 5 ; contador++){      
+    console.log(
+        numero2 + 'x' + contador + '=' + numero2*contador)}
+
+       console.log('===================')
+
+for(let contador = 0; contador <= 5 ; contador++){
+    console.log(
+        numero3 + 'x' + contador + '=' + numero3*contador
+    )}
+
+
